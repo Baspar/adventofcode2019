74641375ac8fcc633529e59f2c8350c45beb1246
Baspar <me@bastien-laine.io>
2019-12-01 16:29:28 +0800

    [add] Day1: Part2

--- rust/src/days/day1.rs
+++ rust/src/days/day1.rs
@@ -12,13 +12,27 @@ pub fn part1 (input: &str) -> String {
         read_input(input)
             .iter()
             .map(|i: &i64| i / 3 - 2)
-            .fold(0i64, |a, b| { a + b as i64 })
+            .fold(0i64, |a, b| { a + b })
     )
 }
 
 // Part2
-pub fn part2 (_input: &str) -> String {
-    format!("{}", _input)
+pub fn part2 (input: &str) -> String {
+    let fuel = read_input(input)
+        .iter()
+        .map(|weight: &i64| {
+            let mut i = *weight;
+            let mut fuel = 0;
+            i = i / 3 - 2;
+            while i > 0 {
+                fuel += i;
+                i = i / 3 - 2;
+            }
+            fuel
+        })
+        .fold(0i64, |a, b| { a + b });
+
+    format!("{}", fuel)
 }
 
 // Tests
@@ -37,6 +51,8 @@ mod tests {
 
     #[test]
     fn day1_part2 () {
-        assert_eq!(0, 0);
+        assert_eq!(super::part2("14"), "2");
+        assert_eq!(super::part2("1969"), "966");
+        assert_eq!(super::part2("100756"), "50346");
     }
 }
