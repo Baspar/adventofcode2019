dfc37138f81935f0d40125c7468fd03ca53608cc
Bastien Laine <me@bastien-laine.io>
2019-12-02 13:33:31 +0800

    [add] Day2: Part2

--- rust/src/days/day2.rs
+++ rust/src/days/day2.rs
@@ -37,8 +37,41 @@ pub fn part1 (input: &str) -> String {
 }
 
 // Part2
-pub fn part2 (_input: &str) -> String {
-    format!("{}", _input)
+pub fn part2 (input: &str) -> String {
+    let expected_output = 19690720;
+    for noun in 0..99 {
+        for verb in 0..99 {
+            let mut opcodes = read_input(input);
+            opcodes[1] = noun;
+            opcodes[2] = verb;
+            let mut pos = 0;
+            loop {
+                match opcodes[pos] {
+                    1 => {
+                        let a = opcodes[pos + 1] as usize;
+                        let b = opcodes[pos + 2] as usize;
+                        let c = opcodes[pos + 3] as usize;
+                        opcodes[c] = opcodes[a] + opcodes[b];
+                        pos += 4;
+                    },
+                    2 => {
+                        let a = opcodes[pos + 1] as usize;
+                        let b = opcodes[pos + 2] as usize;
+                        let c = opcodes[pos + 3] as usize;
+                        opcodes[c] = opcodes[a] * opcodes[b];
+                        pos += 4;
+                    },
+                    99 => { break },
+                    _ => { break }
+                }
+            }
+            let output = opcodes[0];
+            if expected_output == output {
+                return format!("noun = {}, verb = {}", noun, verb);
+            }
+        }
+    }
+    return format!("")
 }
 
 // Tests
