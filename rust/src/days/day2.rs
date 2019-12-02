24d37b62f16924a7e7e915d29fdd1629afb3ed88
Bastien Laine <me@bastien-laine.io>
2019-12-02 14:37:03 +0800

    [edit] Day2: refactor

--- rust/src/days/day2.rs
+++ rust/src/days/day2.rs
@@ -1,52 +1,55 @@
 // Helper
-fn read_input (input: &str) -> Vec<i32> {
+fn read_input (input: &str) -> Vec<usize> {
     input
         .trim()
         .split(",")
         .map(|s: &str| s.parse().unwrap())
         .collect()
 }
-fn main_loop(noun: i32, verb: i32, opcodes: &mut Vec<i32>) {
+fn main_loop(mut opcodes: Vec<usize>, noun: usize, verb: usize) -> usize {
     opcodes[1] = noun;
     opcodes[2] = verb;
     let mut pos = 0;
     loop {
         match opcodes[pos] {
             1 => {
-                let a = opcodes[pos + 1] as usize;
-                let b = opcodes[pos + 2] as usize;
-                let c = opcodes[pos + 3] as usize;
+                let [a, b, c] = [
+                    opcodes[pos + 1],
+                    opcodes[pos + 2],
+                    opcodes[pos + 3]
+                ];
                 opcodes[c] = opcodes[a] + opcodes[b];
-                pos += 4;
             },
             2 => {
-                let a = opcodes[pos + 1] as usize;
-                let b = opcodes[pos + 2] as usize;
-                let c = opcodes[pos + 3] as usize;
+                let [a, b, c] = [
+                    opcodes[pos + 1],
+                    opcodes[pos + 2],
+                    opcodes[pos + 3]
+                ];
                 opcodes[c] = opcodes[a] * opcodes[b];
-                pos += 4;
             },
             99 => { break },
             _ => { break }
         }
+        pos += 4;
     }
+    return opcodes[0];
 }
 
 // Part1
 pub fn part1 (input: &str) -> String {
-    let mut opcodes = read_input(input);
-    main_loop(12, 2, &mut opcodes);
-    format!("{}", opcodes[0])
+    let opcodes = read_input(input);
+    let output = main_loop(opcodes, 12, 2);
+    format!("{}", output)
 }
 
 // Part2
 pub fn part2 (input: &str) -> String {
     let expected_output = 19690720;
+    let opcodes = read_input(input);
     for noun in 0..100 {
         for verb in 0..100 {
-            let mut opcodes = read_input(input);
-            main_loop(noun, verb, &mut opcodes);
-            let output = opcodes[0];
+            let output = main_loop(opcodes.clone(), noun, verb);
             if expected_output == output {
                 return format!("noun = {}, verb = {}", noun, verb);
             }
@@ -60,11 +63,10 @@ pub fn part2 (input: &str) -> String {
 mod tests {
     #[test]
     fn day2_part1 () {
-        assert_eq!(super::part1("1,0,0,0,99"), "2");
+        assert_eq!(super::main_loop(super::read_input("1,0,0,0,99"), 0, 0), 2);
     }
 
     #[test]
     fn day2_part2 () {
-        assert_eq!(0, 0);
     }
 }
