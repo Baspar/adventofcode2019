95fe3b64cac4aee65964019ad5dc3d5905dd84d3
Bastien Laine <me@bastien-laine.io>
2019-12-02 13:36:20 +0800

    [edit] Day2: refactor

--- rust/src/days/day2.rs
+++ rust/src/days/day2.rs
@@ -6,12 +6,9 @@ fn read_input (input: &str) -> Vec<i32> {
         .map(|s: &str| s.parse().unwrap())
         .collect()
 }
-
-// Part1
-pub fn part1 (input: &str) -> String {
-    let mut opcodes = read_input(input);
-    opcodes[1] = 12;
-    opcodes[2] = 2;
+fn main_loop(noun: i32, verb: i32, opcodes: &mut Vec<i32>) {
+    opcodes[1] = noun;
+    opcodes[2] = verb;
     let mut pos = 0;
     loop {
         match opcodes[pos] {
@@ -33,6 +30,12 @@ pub fn part1 (input: &str) -> String {
             _ => { break }
         }
     }
+}
+
+// Part1
+pub fn part1 (input: &str) -> String {
+    let mut opcodes = read_input(input);
+    main_loop(12, 2, &mut opcodes);
     format!("{}", opcodes[0])
 }
 
@@ -42,29 +45,7 @@ pub fn part2 (input: &str) -> String {
     for noun in 0..99 {
         for verb in 0..99 {
             let mut opcodes = read_input(input);
-            opcodes[1] = noun;
-            opcodes[2] = verb;
-            let mut pos = 0;
-            loop {
-                match opcodes[pos] {
-                    1 => {
-                        let a = opcodes[pos + 1] as usize;
-                        let b = opcodes[pos + 2] as usize;
-                        let c = opcodes[pos + 3] as usize;
-                        opcodes[c] = opcodes[a] + opcodes[b];
-                        pos += 4;
-                    },
-                    2 => {
-                        let a = opcodes[pos + 1] as usize;
-                        let b = opcodes[pos + 2] as usize;
-                        let c = opcodes[pos + 3] as usize;
-                        opcodes[c] = opcodes[a] * opcodes[b];
-                        pos += 4;
-                    },
-                    99 => { break },
-                    _ => { break }
-                }
-            }
+            main_loop(noun, verb, &mut opcodes);
             let output = opcodes[0];
             if expected_output == output {
                 return format!("noun = {}, verb = {}", noun, verb);
