f8e346c329e9d1a34bf973f4b8fb97af3b543f60
Bastien Laine <me@bastien-laine.io>
2019-12-04 13:32:54 +0800

    [add] Day4: Part2

--- rust/src/days/day4.rs
+++ rust/src/days/day4.rs
@@ -26,6 +26,33 @@ fn is_valid(n: i64) -> bool {
 
     two_same && never_decrease
 }
+fn is_valid_part2(n: i64) -> bool {
+    let s = format!("{}", n);
+    let chars: Vec<char> = s.chars().collect();
+
+    let mut two_same = false;
+    let mut never_decrease = true;
+    let mut current = (chars.get(0).unwrap(), 1);
+
+    for i in 1..s.len() {
+        let (current_char, current_count) = current;
+        let a = chars.get(i - 1).unwrap();
+        let b = chars.get(i).unwrap();
+        if b == current_char {
+            current = (current_char, current_count + 1)
+        } else {
+            if current_count == 2 { two_same = true }
+            current = (b, 1)
+        }
+        if a > b {
+            never_decrease = false;
+        }
+    }
+    let (_, current_count) = current;
+    if current_count == 2 { two_same = true }
+
+    two_same && never_decrease
+}
 
 // Part1
 pub fn part1 (input: &str) -> String {
@@ -38,7 +65,11 @@ pub fn part1 (input: &str) -> String {
 
 // Part2
 pub fn part2 (input: &str) -> String {
-    format!("{:?}", read_input(input))
+    let (from, to) = read_input(input);
+    let n = (from..=to)
+        .filter(|i| is_valid_part2(*i))
+        .count();
+    return format!("{}", n)
 }
 
 // Tests
@@ -53,6 +84,8 @@ mod tests {
 
     #[test]
     fn day4_part2 () {
-        assert_eq!(super::part2("0"), "0");
+        assert_eq!(super::part2("112233-112233"), "1");
+        assert_eq!(super::part2("123444-123444"), "0");
+        assert_eq!(super::part2("111122-111122"), "1");
     }
 }
