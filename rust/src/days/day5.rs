cdceaf53339ccf09555f5c891c4d9b6cd4730819
Baspar <me@bastien-laine.io>
2019-12-06 00:31:04 +0800

    [edit] Day5: Refactor

--- rust/src/days/day5.rs
+++ rust/src/days/day5.rs
@@ -13,90 +13,72 @@ fn main_loop(mut opcodes: Vec<i64>, system_id: i64) -> i64 {
     let mut pos = 0;
     let mut out = None;
     loop {
-        let opcode = opcodes[pos] % 100;
-        let mode_param_1 = opcodes[pos] / 100 % 10;
-        let mode_param_2 = opcodes[pos] / 1000 % 10;
-        // let mode_param_3 = opcodes[pos] / 10000 % 10;
-        match opcode {
+        let mode = |shift: u32| {
+            opcodes[pos] / 10i64.pow(shift + 1) % 10
+        };
+        let get_param = |shift: u32, mode| -> i64 {
+            let value = opcodes[pos + shift as usize];
+            if mode == 0 {
+                return opcodes[value as usize];
+            } else {
+                return value;
+            }
+        };
+        match opcodes[pos] % 100 {
             1 => { // Addition
-                let [a, b, c] = [
-                    opcodes[pos + 1],
-                    opcodes[pos + 2],
-                    opcodes[pos + 3]
-                ];
-                let value_1 = if mode_param_1 == 0 { opcodes[a as usize] } else { a };
-                let value_2 = if mode_param_2 == 0 { opcodes[b as usize] } else { b };
-                opcodes[c as usize] = value_1 + value_2;
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                let c = get_param(3, 1);
+                opcodes[c as usize] = a + b;
                 pos += 4;
             },
             2 => { // Multiplication
-                let [a, b, c] = [
-                    opcodes[pos + 1],
-                    opcodes[pos + 2],
-                    opcodes[pos + 3]
-                ];
-                let value_1 = if mode_param_1 == 0 { opcodes[a as usize] } else { a };
-                let value_2 = if mode_param_2 == 0 { opcodes[b as usize] } else { b };
-                opcodes[c as usize] = value_1 * value_2;
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                let c = get_param(3, 1);
+                opcodes[c as usize] = a * b;
                 pos += 4;
             },
             3 => { // Input
-                let a = opcodes[pos + 1];
+                let a = get_param(1, 1);
                 opcodes[a as usize] = system_id;
                 pos += 2;
             },
             4 => { // Output
-                let a = opcodes[pos + 1];
-                let value_1 = if mode_param_1 == 0 { opcodes[a as usize] } else { a };
-                out = Some(value_1);
+                let a = get_param(1, mode(1));
+                out = Some(a);
                 pos += 2;
             },
             5 => { //jump-if-true
-                let [a, b] = [
-                    opcodes[pos + 1],
-                    opcodes[pos + 2]
-                ];
-                let value_1 = if mode_param_1 == 0 { opcodes[a as usize] } else { a };
-                let value_2 = if mode_param_2 == 0 { opcodes[b as usize] } else { b };
-                if value_1 != 0 {
-                    pos = value_2 as usize;
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                if a != 0 {
+                    pos = b as usize;
                 } else {
                     pos += 3;
                 }
             },
             6 => { //jump-if-false
-                let [a, b] = [
-                    opcodes[pos + 1],
-                    opcodes[pos + 2]
-                ];
-                let value_1 = if mode_param_1 == 0 { opcodes[a as usize] } else { a };
-                let value_2 = if mode_param_2 == 0 { opcodes[b as usize] } else { b };
-                if value_1 == 0 {
-                    pos = value_2 as usize;
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                if a == 0 {
+                    pos = b as usize;
                 } else {
                     pos += 3;
                 }
             },
             7 => { //less-than
-                let [a, b, c] = [
-                    opcodes[pos + 1],
-                    opcodes[pos + 2],
-                    opcodes[pos + 3]
-                ];
-                let value_1 = if mode_param_1 == 0 { opcodes[a as usize] } else { a };
-                let value_2 = if mode_param_2 == 0 { opcodes[b as usize] } else { b };
-                opcodes[c as usize] = if value_1 < value_2 {1} else {0};
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                let c = get_param(3, 1);
+                opcodes[c as usize] = if a < b {1} else {0};
                 pos += 4;
             },
             8 => { //equals
-                let [a, b, c] = [
-                    opcodes[pos + 1],
-                    opcodes[pos + 2],
-                    opcodes[pos + 3]
-                ];
-                let value_1 = if mode_param_1 == 0 { opcodes[a as usize] } else { a };
-                let value_2 = if mode_param_2 == 0 { opcodes[b as usize] } else { b };
-                opcodes[c as usize] = if value_1 == value_2 {1} else {0};
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                let c = get_param(3, 1);
+                opcodes[c as usize] = if a == b {1} else {0};
                 pos += 4;
             },
             99 => { // Exit
