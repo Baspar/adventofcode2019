9d5f97f4077867469c0dfb097c41cea3b8e57daf
Baspar <me@bastien-laine.io>
2019-12-09 01:37:37 +0800

    [add] Day8: Part1

--- rust/src/days/day8.rs
+++ rust/src/days/day8.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 // Helper
 fn read_input (_input: &str) -> i32 {
     0
@@ -5,7 +7,29 @@ fn read_input (_input: &str) -> i32 {
 
 // Part1
 pub fn part1 (input: &str) -> String {
-    format!("{}", read_input(input))
+    let nb_pixel = 25 * 6;
+
+    let mut min_0 = i64::max_value();
+    let mut output = 0;
+
+    for layer in input.trim().chars().chunks(nb_pixel).into_iter() {
+        let mut count_0 = 0;
+        let mut count_1 = 0;
+        let mut count_2 = 0;
+        for c in layer {
+            match c {
+                '0' => count_0 += 1,
+                '1' => count_1 += 1,
+                '2' => count_2 += 1,
+                _ => {}
+            }
+        }
+        if count_0 < min_0 {
+            output = count_1 * count_2;
+            min_0 = count_0;
+        }
+    }
+    format!("{}", output)
 }
 
 // Part2
@@ -18,7 +42,7 @@ pub fn part2 (input: &str) -> String {
 mod tests {
     #[test]
     fn day8_part1 () {
-        assert_eq!(super::part1("0"), "0");
+        assert_eq!(super::part1("1234567890120"), "0");
     }
 
     #[test]
