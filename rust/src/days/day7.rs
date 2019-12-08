3c463b3042dc35076854a6be7a5be88b51b19609
Baspar <me@bastien-laine.io>
2019-12-08 23:22:02 +0800

    [edit] Day7: Refactor Part1

--- rust/src/days/day7.rs
+++ rust/src/days/day7.rs
@@ -1,5 +1,127 @@
 use itertools::Itertools;
+use std::collections::VecDeque;
 use std::cmp;
+
+type Opcodes = Vec<i64>;
+
+enum Status {
+    Ok,
+    Output(i64),
+    WaitingForInput,
+    Halt
+}
+
+struct Amplifier {
+    pos: usize,
+    opcodes: Opcodes,
+    input: VecDeque<i64>
+}
+impl Amplifier {
+    fn new (opcodes: &Opcodes) -> Self {
+        Self {
+            pos: 0,
+            opcodes: opcodes.clone(),
+            input: VecDeque::new()
+        }
+    }
+    fn add_input (self: &mut Self, i: i64) {
+        self.input.push_back(i);
+    }
+    fn step (self: &mut Self) -> Status {
+        let mode = |shift: u32| {
+            self.opcodes[self.pos] / 10i64.pow(shift + 1) % 10
+        };
+        let get_param = |shift: u32, mode| -> i64 {
+            let value = self.opcodes[self.pos + shift as usize];
+            if mode == 0 {
+                return self.opcodes[value as usize];
+            } else {
+                return value;
+            }
+        };
+        match self.opcodes[self.pos] % 100 {
+            1 => { // Addition
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                let c = get_param(3, 1);
+                self.opcodes[c as usize] = a + b;
+                self.pos += 4;
+            },
+            2 => { // Multiplication
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                let c = get_param(3, 1);
+                self.opcodes[c as usize] = a * b;
+                self.pos += 4;
+            },
+            3 => { // Input
+                let a = get_param(1, 1);
+                if self.input.is_empty() {
+                    return Status::WaitingForInput;
+                }
+
+                self.opcodes[a as usize] = self.input.pop_front().unwrap();
+
+                self.pos += 2;
+            },
+            4 => { // Output
+                let a = get_param(1, mode(1));
+                self.pos += 2;
+                return Status::Output(a)
+            },
+            5 => { //jump-if-true
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                if a != 0 {
+                    self.pos = b as usize;
+                } else {
+                    self.pos += 3;
+                }
+            },
+            6 => { //jump-if-false
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                if a == 0 {
+                    self.pos = b as usize;
+                } else {
+                    self.pos += 3;
+                }
+            },
+            7 => { //less-than
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                let c = get_param(3, 1);
+                self.opcodes[c as usize] = if a < b {1} else {0};
+                self.pos += 4;
+            },
+            8 => { //equals
+                let a = get_param(1, mode(1));
+                let b = get_param(2, mode(2));
+                let c = get_param(3, 1);
+                self.opcodes[c as usize] = if a == b {1} else {0};
+                self.pos += 4;
+            },
+            99 => { // Exit
+                return Status::Halt;
+            },
+            _ => { // Error
+                return Status::Halt;
+            }
+        }
+
+        Status::Ok
+    }
+    fn run_until_interrupted (self: &mut Self) -> Status {
+        loop {
+            match self.step() {
+                Status::Ok => {}
+                Status::Output(i) => { return Status::Output(i) }
+                Status::WaitingForInput => { return Status::WaitingForInput }
+                Status::Halt => { return Status::Halt }
+            }
+        }
+    }
+}
 
 // Helper
 fn read_input (input: &str) -> Vec<i64> {
@@ -108,7 +230,13 @@ pub fn part1 (input: &str) -> String {
     for permutation in (0..5).permutations(5) {
         let mut output = 0;
         for phase in permutation {
-            output = main_loop(opcodes.clone(), phase, output);
+            let mut amplifier = Amplifier::new(&opcodes);
+            amplifier.add_input(phase);
+            amplifier.add_input(output);
+            match amplifier.run_until_interrupted() {
+                Status::Output(i) => output = i,
+                _ => {}
+            }
         }
 
         max_output = cmp::max(max_output, output);
@@ -119,9 +247,18 @@ pub fn part1 (input: &str) -> String {
 
 // Part2
 pub fn part2 (input: &str) -> String {
-    read_input(input);
-    // let output = main_loop(opcodes, 5);
-    format!("{}", "0")
+    let opcodes = read_input(input);
+    let mut max_output = 0;
+    for permutation in (5..10).permutations(5) {
+        let mut output = 0;
+        for phase in permutation {
+            output = main_loop(opcodes.clone(), phase, output);
+        }
+
+        max_output = cmp::max(max_output, output);
+    }
+
+    format!("{}", max_output)
 }
 
 // Tests
