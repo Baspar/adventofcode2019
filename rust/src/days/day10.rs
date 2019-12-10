8c5923ecbf9284a9d618a42884bb61214fb17255
Baspar <me@bastien-laine.io>
2019-12-11 00:51:30 +0800

    [add] Day10: Part2

--- rust/src/days/day10.rs
+++ rust/src/days/day10.rs
@@ -4,6 +4,7 @@ use num::Complex;
 
 // Helper
 type Coord = Complex<i64>;
+type Angle = Coord;
 type Coords = Vec<Coord>;
 
 fn read_input (input: &str) -> Coords {
@@ -28,12 +29,9 @@ fn gcd(x: i64, y: i64) -> i64 {
     }
     x
 }
-
-// Part1
-pub fn part1 (input: &str) -> String {
-    let asteroids = read_input(input);
+fn get_best_asteroid (asteroids: &Coords) -> (i64, Coord) {
     let mut asteroids_set = HashSet::new();
-    for asteroid in &asteroids {
+    for asteroid in asteroids {
         asteroids_set.insert(asteroid);
     }
     let mut nb_reachable: HashMap<Coord, i64> = HashMap::new();
@@ -41,12 +39,10 @@ pub fn part1 (input: &str) -> String {
     'combination_loop: for perm in asteroids.iter().combinations(2) {
         let (asteroid_from, asteroid_to) = (perm[0], perm[1]);
         let distance = asteroid_from - asteroid_to;
-        let gcd = gcd(distance.re.abs(), distance.im.abs());
-        let step = distance / gcd;
+        let divider = gcd(distance.re.abs(), distance.im.abs());
+        let step = distance / divider;
 
-        let mut current = asteroid_to.clone();
-        while { current += step; current != *asteroid_from } {
-            if asteroids_set.contains(&current) {
+        for i in 1..divider {
             if asteroids_set.contains(&(asteroid_to + i * step)) {
                 continue 'combination_loop;
             }
@@ -56,37 +52,71 @@ pub fn part1 (input: &str) -> String {
         *nb_reachable.entry(*asteroid_to).or_insert(0) += 1;
     }
 
-    let max = nb_reachable.values().fold(0, |v1, &v2| std::cmp::max(v1, v2));
+    let mut max = 0;
+    let mut argmax = Complex::new(0, 0);
+    for (asteroid, reachable) in nb_reachable {
+        if reachable > max {
+            max = reachable;
+            argmax = asteroid;
+        }
+    }
+    (max, argmax)
+}
+
+// Part1
+pub fn part1 (input: &str) -> String {
+    let asteroids = read_input(input);
+    let (max, _) = get_best_asteroid(&asteroids);
     format!("{:?}", max)
 }
 
 // Part2
 pub fn part2 (input: &str) -> String {
     let asteroids = read_input(input);
-    let mut asteroids_set = HashSet::new();
-    for asteroid in &asteroids {
-        asteroids_set.insert(asteroid);
+
+    let (_, base) = get_best_asteroid(&asteroids);
+
+    let mut relative_positions: HashMap<Angle, Vec<(i64, Coord)>> = HashMap::new();
+    for asteroid in asteroids {
+        if asteroid != base {
+            let distance = asteroid - base;
+            let divider = gcd(distance.re.abs(), distance.im.abs());
+            let step = distance / divider;
+
+            relative_positions
+                .entry(step).or_insert(Vec::new())
+                .push((divider, asteroid));
+        }
     }
-    let mut nb_reachable: HashMap<Coord, i64> = HashMap::new();
 
-    'combination_loop: for perm in asteroids.iter().combinations(2) {
-        let (asteroid_from, asteroid_to) = (perm[0], perm[1]);
-        let distance = asteroid_from - asteroid_to;
-        let gcd = gcd(distance.re.abs(), distance.im.abs());
-        let step = distance / gcd;
+    let mut sorted_horizons: Vec<(f64, Coords)> = Vec::new();
+    for (Complex {re, im}, mut aligned_position) in relative_positions {
+        let angle = Complex::new(im as f64, re as f64).arg();
+        aligned_position.sort_by_key(|(distance, _)| distance.abs());
+        aligned_position.reverse();
+        let aligned_asteroids: Coords = aligned_position.iter().map(|(_, coord)| *coord).collect();
+        sorted_horizons.push((angle, aligned_asteroids));
+    }
+    sorted_horizons.sort_by(|(angle_a, _), (angle_b, _)| angle_b.partial_cmp(&angle_a).unwrap_or(std::cmp::Ordering::Equal));
 
-        for i in 1..gcd {
-            if asteroids_set.contains(&(asteroid_to + i * step)) {
-                continue 'combination_loop;
+    let mut coords: Vec<Coords> = sorted_horizons.iter().map(|(_, coords)| coords.clone()).collect();
+
+    let mut i = 0;
+    while !coords.is_empty() {
+        let index = i % coords.len();
+
+        match coords[index].pop() {
+            None => { coords.remove(index); },
+            Some(coord) => {
+                if i == 199 {
+                    return format!("{}", 100 * coord.re + coord.im);
+                }
+                i += 1;
             }
         }
 
-        *nb_reachable.entry(*asteroid_from).or_insert(0) += 1;
-        *nb_reachable.entry(*asteroid_to).or_insert(0) += 1;
     }
-
-    let max = nb_reachable.values().fold(0, |v1, &v2| std::cmp::max(v1, v2));
-    format!("{:?}", max)
+    format!("{}", 1)
 }
 
 // Tests
@@ -118,6 +148,6 @@ mod tests {
 
     #[test]
     fn day10_part2 () {
-        assert_eq!(super::part2("0"), "0");
+        assert_eq!(super::part2(".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##"), "802");
     }
 }
