d115363994dec921e332d41a6cd9fe9328d06a2a
Baspar <me@bastien-laine.io>
2019-12-13 01:28:35 +0800

    [add] Day12: Part2

--- rust/src/days/day12.rs
+++ rust/src/days/day12.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 use itertools::Itertools;
+use num::integer::lcm;
 
 // Helper
 fn read_input (input: &str) -> Vec<Vec<i64>> {
@@ -16,34 +17,31 @@ fn read_input (input: &str) -> Vec<Vec<i64>> {
         })
         .collect()
 }
-fn run (mut moons: Vec<Vec<i64>>, n: i64) -> String {
-    let mut velocities = vec![vec![0, 0, 0],vec![0, 0, 0],vec![0, 0, 0],vec![0, 0, 0]];
-    for _ in 0..n {
-        for perm in (0..4).into_iter().combinations(2) {
-            let [i1, i2] = [perm[0], perm[1]];
-            for coord in 0..3 {
-                let coord1 = moons[i1][coord];
-                let coord2 = moons[i2][coord];
-                if coord1 < coord2 {
-                    velocities[i1][coord] += 1; velocities[i2][coord] -= 1;
-                } else if coord1 > coord2 {
-                    velocities[i1][coord] -= 1; velocities[i2][coord] += 1;
-                }
-            }
-        }
-
-        for i in 0..4 {
-            for coord in 0..3 {
-                moons[i][coord] += velocities[i][coord];
+fn step (moons: &mut Vec<Vec<i64>>, velocities: &mut Vec<Vec<i64>>) {
+    for perm in (0..4).into_iter().combinations(2) {
+        let [i1, i2] = [perm[0], perm[1]];
+        for coord in 0..3 {
+            let coord1 = moons[i1][coord];
+            let coord2 = moons[i2][coord];
+            if coord1 < coord2 {
+                velocities[i1][coord] += 1; velocities[i2][coord] -= 1;
+            } else if coord1 > coord2 {
+                velocities[i1][coord] -= 1; velocities[i2][coord] += 1;
             }
         }
     }
-    println!("Moon 1: {:?} {:?}", moons[0], velocities[0]);
-    println!("Moon 2: {:?} {:?}", moons[1], velocities[1]);
-    println!("Moon 3: {:?} {:?}", moons[2], velocities[2]);
-    println!("Moon 4: {:?} {:?}", moons[3], velocities[3]);
-    println!("");
-    println!("");
+
+    for i in 0..4 {
+        for coord in 0..3 {
+            moons[i][coord] += velocities[i][coord];
+        }
+    }
+}
+fn run_for (mut moons: Vec<Vec<i64>>, n: i64) -> String {
+    let mut velocities = vec![vec![0, 0, 0],vec![0, 0, 0],vec![0, 0, 0],vec![0, 0, 0]];
+    for _ in 0..n {
+        step(&mut moons, &mut velocities);
+    }
 
     let mut energy: i64 = 0;
     for i in 0..4 {
@@ -61,13 +59,32 @@ fn run (mut moons: Vec<Vec<i64>>, n: i64) -> String {
 // Part1
 pub fn part1 (input: &str) -> String {
     let moons = read_input(input);
-    run(moons, 1000)
+    run_for(moons, 1000)
 }
 
 // Part2
 pub fn part2 (input: &str) -> String {
-    let moons = read_input(input);
-    format!("{}", 0)
+    let mut moons = read_input(input);
+    let initial_positions = moons.clone();
+    let mut velocities = vec![vec![0, 0, 0],vec![0, 0, 0],vec![0, 0, 0],vec![0, 0, 0]];
+    let mut periods: Vec<Option<i64>> = vec![None, None, None];
+    let mut iteration = 0;
+    while !periods.iter().all(|x| x.is_some()) {
+        step(&mut moons, &mut velocities);
+        iteration += 1;
+        for axis in 0..3 {
+            if periods[axis].is_none() {
+                let loop_detected = (0..4)
+                    .into_iter()
+                    .all(|i| moons[i][axis] == initial_positions[i][axis] && velocities[i][axis] == 0);
+                if loop_detected {
+                    periods[axis] = Some(iteration);
+                }
+            }
+        }
+    }
+    let out = lcm(lcm(periods[0].unwrap(), periods[1].unwrap()), periods[2].unwrap());
+    format!("{}", out)
 }
 
 // Tests
@@ -75,11 +92,11 @@ pub fn part2 (input: &str) -> String {
 mod tests {
     #[test]
     fn day12_part1 () {
-        assert_eq!(super::run(super::read_input("<x=-1, y=0, z=2>
+        assert_eq!(super::run_for(super::read_input("<x=-1, y=0, z=2>
 <x=2, y=-10, z=-7>
 <x=4, y=-8, z=8>
 <x=3, y=5, z=-1>"), 10), "179");
-        assert_eq!(super::run(super::read_input("<x=-8, y=-10, z=0>
+        assert_eq!(super::run_for(super::read_input("<x=-8, y=-10, z=0>
 <x=5, y=5, z=10>
 <x=2, y=-7, z=3>
 <x=9, y=-8, z=-3>"), 100), "1940");
@@ -87,6 +104,13 @@ mod tests {
 
     #[test]
     fn day12_part2 () {
-        assert_eq!(super::part2("0"), "0");
+        assert_eq!(super::part2("<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>"), "2772");
+        assert_eq!(super::part2("<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>"), "4686774924");
     }
 }
