152f22afb3ad0a96114bc1e833f31e1f5c124cc0
Bastien Laine <me@bastien-laine.io>
2019-12-17 12:24:27 +0800

    [add] Day14: Part1

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,20 +1,103 @@
-use regex::Regex;
+use std::collections::HashMap;
+
+type Quantity = (i64, String);
+type Quantities = Vec<Quantity>;
+type Recipe = (Quantities, i64, String);
+
+struct RecipeBook {
+    recipes: HashMap<String, (i64, Quantities)>,
+    leftovers: HashMap<String, i64>,
+    pub ore_needed: i64
+}
+impl RecipeBook {
+    pub fn new (r: Vec<Recipe>) -> Self {
+        let mut recipes = HashMap::new();
+
+        for (material_needed, quantity_created, material_created) in r {
+            recipes.insert(material_created, (quantity_created, material_needed));
+        }
+
+        Self {
+            recipes,
+            leftovers: HashMap::new(),
+            ore_needed: 0
+        }
+    }
+
+    fn run (self: &mut Self) {
+        let mut needed = Vec::new();
+        needed.push((1, "FUEL"));
+
+        while !needed.is_empty() {
+            let (mut quantity_to_create, material_to_create) = needed.pop().unwrap();
+
+            // Used leftovers
+            let claimable = *self.leftovers.get(material_to_create).unwrap_or(&0).min(&quantity_to_create);
+            quantity_to_create -= claimable;
+            if claimable != 0 {
+                *self.leftovers.get_mut(material_to_create).unwrap() -= claimable;
+            }
+
+            // Use recipes
+            if quantity_to_create != 0 {
+                if material_to_create == "ORE" {
+                    self.ore_needed += quantity_to_create;
+                    continue;
+                }
+                let (batch_size, materials_needed) = self.recipes.get(material_to_create).expect("Recipe not found");
+
+                let nb_batch: i64 = (quantity_to_create - 1) / batch_size + 1;
+                let leftovers = nb_batch * batch_size - quantity_to_create;
+                if leftovers != 0 {
+                    *self.leftovers.entry(String::from(material_to_create)).or_insert(0) += leftovers;
+                }
+
+                for (quantity_needed, material_needed) in materials_needed {
+                    needed.push((quantity_needed * nb_batch, material_needed));
+                }
+            }
+        }
+    }
+}
 
 // Helper
-fn read_input (input: &str) -> i32 {
-    let params: Vec<Vec<&str>> = input.split(" => ").map(|s: &str| s.trim().split(",").map(|s| s.trim()).collect()).collect();
-    // let re = Regex::new(r"")
-    0
+fn read_quantities (s: &str) -> Recipe {
+    let values: Vec<Quantities> = s
+        .split(" => ")
+        .map(|s| -> Quantities {
+            s.split(",")
+                .map(|s| -> Quantity {
+                    let params: Vec<&str> = s.trim().split(" ").collect();
+                    let value: i64 = params[0].trim().parse().expect("To be an i64");
+                    let material = params[1].trim();
+                    (value, String::from(material))
+                })
+            .collect()
+        })
+        .collect();
+    let from = values[0].clone();
+    let to = values[1][0].clone();
+    (from, to.0, to.1)
+}
+
+fn read_input (input: &str) -> Vec<Recipe> {
+    input
+        .lines()
+        .map(read_quantities)
+        .collect()
 }
 
 // Part1
 pub fn part1 (input: &str) -> String {
-    format!("{}", read_input(input))
+    let recipes = read_input(input);
+    let mut recipe_book = RecipeBook::new(recipes);
+    recipe_book.run();
+    format!("{}", recipe_book.ore_needed)
 }
 
 // Part2
-pub fn part2 (input: &str) -> String {
-    format!("{}", read_input(input))
+pub fn part2 (_input: &str) -> String {
+    format!("{}", 0)
 }
 
 // Tests
@@ -22,7 +105,57 @@ pub fn part2 (input: &str) -> String {
 mod tests {
     #[test]
     fn day14_part1 () {
-        assert_eq!(super::part1("0"), "0");
+        assert_eq!(super::part1("10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL"), "31");
+        assert_eq!(super::part1("9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL"), "165");
+        assert_eq!(super::part1("157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"), "13312");
+        assert_eq!(super::part1("2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF"), "180697");
+        assert_eq!(super::part1("171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX"), "2210736");
     }
 
     #[test]
