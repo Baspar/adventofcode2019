7eb383d40755e2a89eca589410513dee8d8bdd6f
Bastien Laine <me@bastien-laine.io>
2019-12-23 19:03:26 +0800

    [wip] Day17: Part2

--- src/days/day17.rs
+++ src/days/day17.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use num::Complex;
 use crate::intcode::{Opcodes,Status,Machine};
 
 // Helper
@@ -12,37 +13,67 @@ fn read_input (input: &str) -> Opcodes {
         .collect()
 }
 struct Map {
+    bot_pos: Complex<i64>,
+    bot_orientation: Complex<i64>,
     max_x: i64,
     max_y: i64,
-    map: HashMap<(i64, i64), char>
+    map: HashMap<Complex<i64>, char>
 }
 impl Map {
-    fn new (mut machine: Machine) -> Self{
-        let mut map: HashMap<(i64, i64), char> = HashMap::new();
-        let mut pos = (0, 0);
+    fn new (mut machine: Machine) -> Self {
+        let mut map: HashMap<Complex<i64>, char> = HashMap::new();
+        let mut pos = Complex::new(0, 0);
         let mut max_x = 0;
         let mut max_y = 0;
+        let mut bot_pos = Complex::new(0, 0);
+        let mut bot_orientation = Complex::new(0, 0);
         loop {
             match machine.step() {
                 Status::Halt => break,
                 Status::Output(10) => {
-                    pos.1 += 1;
-                    pos.0 = 0;
+                    pos.re = 0;
+                    pos.im += 1;
                 },
                 Status::Output(x) => {
-                    max_x = max_x.max(pos.0);
-                    max_y = max_y.max(pos.1);
-                    map.insert(pos, x as u8 as char);
-                    pos.0 += 1;
+                    let c = x as u8 as char;
+                    match c {
+                        '^' => {
+                            bot_pos = pos;
+                            bot_orientation = Complex::new(0, -1);
+                        },
+                        'V' => {
+                            bot_pos = pos;
+                            bot_orientation = Complex::new(0, 1);
+                        },
+                        '<' => {
+                            bot_pos = pos;
+                            bot_orientation = Complex::new(-1, 0);
+                        },
+                        '>' => {
+                            bot_pos = pos;
+                            bot_orientation = Complex::new(1, 0);
+                        },
+                        _   => { }
+                    };
+                    max_x = max_x.max(pos.re);
+                    max_y = max_y.max(pos.im);
+                    match c {
+                        '.' => map.insert(pos, '.'),
+                        _   => map.insert(pos, '#')
+                    };
+                    pos.re += 1;
                 },
                 Status::Ok => {},
                 err => { println!("{:?}", err) }
             }
         }
+
         Self {
             map,
             max_y,
-            max_x
+            max_x,
+            bot_orientation,
+            bot_pos
         }
     }
 
@@ -51,26 +82,73 @@ impl Map {
         println!("");
         for y in 0..=self.max_y {
             for x in 0..=self.max_x {
-                print!("{}", self.map[&(x, y)]);
+                if Complex::new(x, y) == self.bot_pos {
+                    match (self.bot_orientation.re, self.bot_orientation.im) {
+                        (0, -1) => print!("^"),
+                        (0, 1)  => print!("V"),
+                        (-1, 0) => print!("<"),
+                        (1, 0)  => print!(">"),
+                        _       => panic!("Unknown")
+                    }
+                } else {
+                    print!("{}", self.map[&Complex::new(x, y)]);
+                }
             }
             println!("");
         }
     }
 
-    fn part1(self: &Self) -> i64 {
+    fn part1 (self: &Self) -> i64 {
         let mut out = 0;
-        for ((x, y), c) in &self.map {
+        for (Complex {re: x, im: y}, c) in &self.map {
             if *c == '.' { continue }
             if
-                self.map.get(&(*x + 1, *y)) == Some(&'#') &&
-                self.map.get(&(*x - 1, *y)) == Some(&'#') &&
-                self.map.get(&(*x, *y + 1)) == Some(&'#') &&
-                self.map.get(&(*x, *y - 1)) == Some(&'#')
+                self.map.get(&Complex::new(*x + 1, *y)) == Some(&'#') &&
+                self.map.get(&Complex::new(*x - 1, *y)) == Some(&'#') &&
+                self.map.get(&Complex::new(*x, *y + 1)) == Some(&'#') &&
+                self.map.get(&Complex::new(*x, *y - 1)) == Some(&'#')
             {
                 out += x * y
             }
         }
         out
+    }
+
+    fn get_instruction (self: &mut Self) -> String {
+        let mut out = Vec::new();
+        loop {
+            let left_orientation = Complex::new(0, -1) * self.bot_orientation;
+            let left_pos = self.bot_pos + left_orientation;
+            let left_value = self.map.get(&left_pos);
+            let right_orientation = Complex::new(0, 1) * self.bot_orientation;
+            let right_pos = self.bot_pos + right_orientation;
+            let right_value = self.map.get(&right_pos);
+            match (left_value, right_value) {
+                (Some('#'), _) => {
+                    out.push(String::from("L"));
+                    self.bot_orientation = left_orientation;
+                },
+                (_, Some('#')) => {
+                    out.push(String::from("R"));
+                    self.bot_orientation = right_orientation;
+                },
+                _ => break
+            }
+
+            let mut i = 0;
+            while self.map.get(&(self.bot_pos + self.bot_orientation)) == Some(&'#') {
+                self.bot_pos += self.bot_orientation;
+                i += 1;
+            }
+
+            out.push(i.to_string());
+
+            self.display()
+        }
+
+        out
+            .iter()
+            .fold(String::from(""), |a, b| a + b + ",")
     }
 }
 
@@ -86,8 +164,22 @@ pub fn part1 (input: &str) -> String {
 
 // Part2
 pub fn part2 (input: &str) -> String {
-    let opcodes = read_input(input);
-    let machine = Machine::new(&opcodes);
+    let mut opcodes = read_input(input);
+    // opcodes[0] = 2;
+    // R12L8L4L4L8R6L6
+    // R12L8L4L4L8R6L6
+    // L8L4R12L6L4R12L8L4L4
+    // L8L4R12L6L4R12L8L4L4
+    // L8L4R12L6L4L8R6L6
+    //
+    // R12L8L4L4L8R6L6
+    // L8L4R12L6L4R12L8L4L4
+    // L8L4R12L6L4L8R6L6
+
+    let mut map = Map::new(Machine::new(&opcodes));
+    map.display();
+    let instructions = map.get_instruction();
+
     format!("{}", 0)
 }
 
@@ -101,6 +193,6 @@ mod tests {
 
     #[test]
     fn day17_part2 () {
-        // assert_eq!(super::part2("0"), "0");
+        assert_eq!(super::part2("104,35,104,35,104,35,104,35,104,35,104,35,104,35,104,46,104,46,104,46,104,35,104,35,104,35,104,35,104,35,104,10,104,35,104,46,104,46,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,10,104,35,104,46,104,46,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,10,104,46,104,46,104,46,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,10,104,46,104,46,104,46,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,35,104,35,104,46,104,35,104,10,104,46,104,46,104,46,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,46,104,46,104,35,104,46,104,35,104,10,104,94,104,35,104,35,104,35,104,35,104,35,104,35,104,35,104,35,104,46,104,46,104,46,104,35,104,46,104,35,104,10,104,46,104,46,104,46,104,46,104,46,104,46,104,35,104,46,104,35,104,46,104,46,104,46,104,35,104,46,104,35,104,10,104,46,104,46,104,46,104,46,104,46,104,46,104,35,104,35,104,35,104,35,104,35,104,35,104,35,104,35,104,35,104,10,104,46,104,46,104,46,104,46,104,46,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,46,104,46,104,10,104,46,104,46,104,46,104,46,104,35,104,35,104,35,104,35,104,35,104,35,104,35,104,35,104,35,104,46,104,46,104,10,104,46,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,46,104,46,104,46,104,10,104,46,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,46,104,46,104,46,104,10,104,46,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,35,104,46,104,46,104,46,104,46,104,46,104,46,104,10,104,46,104,46,104,46,104,46,104,35,104,35,104,35,104,35,104,35,104,46,104,46,104,46,104,46,104,46,104,46,99"), "");
     }
 }
